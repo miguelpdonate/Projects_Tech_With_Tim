816a6f438b1d178542a7d901afb8d760389b06de
Miguel Perez <miguel.p.donate@gmail.com>
2023-09-18 19:50:35 +0200

    finished project pig

--- Pig_Porject1.py
+++ Pig_Porject1.py
@@ -27,18 +27,33 @@ max_score = 50
 player_scores = [0 for _ in range(players)]
 
 while max(player_scores) < max_score:
-    should_roll = input("Would you like to roll (y)? ")
-    if should_roll.lower() != "y":
-        break
+    
+    for player_idx in range(players):
+        print("\nPlayer number", player_idx + 1, "turn has just started!")
+        print("Your toal score is:", player_scores[player_idx], "\n")
+        current_score = 0
 
-    value = roll()
-    if value == 1:
-        print("You rolled a 1! Turn over!")
-    else:
-        print("You rolled a:", value) 
+        while True:
+            should_roll = input("Would you like to roll (y)? ")
+            if should_roll.lower() != "y":
+                break
 
+            value = roll()
+            if value == 1:
+                print("You rolled a 1! Turn over!")
+            else:
+                current_score += value
+                print("You rolled a:", value)
 
+            print("Your score is :", current_score) 
 
+        player_scores[player_idx] += current_score
+        print("Your toal score is:", player_scores[player_idx])
+
+max_score = max(player_scores)
+winning_idx = player_scores.index(max_score)
+print("Player number", winning_idx + 1,
+      "is the winner with a score of:", max_score)
 
 
 
